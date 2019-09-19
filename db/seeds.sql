3bcbd4318729053bb2842e3494cbea92481e2a5a
Austin Alber <austinalber@gmail.com>
2019-09-19 15:17:02 -0400

    Added initial files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon and Egg Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Chicken Burger', true);
