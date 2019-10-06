d805d22fff389b33096e5df9f8d95965068cf8c4
Austin Alber <austinalber@gmail.com>
2019-10-06 16:18:36 -0400

    added .env

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ var mysql = require("mysql");
 
 
 if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
+    var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
